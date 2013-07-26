638d9ab46dd8157acb291e8668a46b1d6fe6034e
Andy Woo <wookie@crazylabel.com>
2013-07-26 14:10:05 +0800

    Initial Commit

---
+++ ToyResolution/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToyResolution
+//
+//  Created by Andy Woo on 26/7/13.
+//  Copyright (c) 2013 WOOKIEWEB.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToyResolution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToyResolution
+//
+//  Created by Andy Woo on 26/7/13.
+//  Copyright (c) 2013 WOOKIEWEB.COM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
